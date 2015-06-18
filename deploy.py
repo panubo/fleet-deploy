72ecbfd3f259874c795d6e2f49a3b16927a332ce
Andrew Cutler <macropin@gmail.com>
2015-06-18 13:09:38 +1000

    WIP commit. New approach to implementing deployment managers. Support for definable chunking.

--- deploy.py
+++ deploy.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 from time import sleep
 import math
-import sys
 
 import click
 import fleet.v1 as fleet
@@ -24,127 +21,254 @@ class FleetConnection(object):
 class Instance(object):
     """ Unit Instance """
 
-    def __init__(self, fleet_client, name):
-        self.fleet = fleet_client
+    def __init__(self, name, state):
         self.name = name
+        self.state = state  # inactive, launched
 
     def __str__(self):
         return self.name
 
     @property
     def details(self):
-        return "%s %s" % (self, self.get_state())
+        return "%s %s" % (self, self.state)
 
-    def start(self):
-        return self.fleet.set_unit_desired_state(self.name, 'launched')
 
-    def stop(self):
-        return self.fleet.set_unit_desired_state(self.name, 'inactive')
+class Step(object):
+    """ Single Step in a Deployment Plan """
 
-    def get_state(self):
-        """ Return current state """
-        for unit in self.fleet.list_unit_states():
-            if unit.name == self.name:
-                return unit.systemdSubState
+    def __init__(self, action, name):
+        self.action = action
+        self.name = name
 
-    @property
-    def is_running(self):
-        if self.get_state() == 'running':
-            return True
-        else:
-            return False
+    def __str__(self):
+        return "%s %s" % (self.action, self.name)
+
+
+class PlanManager(object):
+
+    def __init__(self, fleet_client, steps):
+        self.fleet = fleet_client
+        self.steps = steps
+
+    def __str__(self):
+        return "StepManager %s" % len(self.steps)
+
+    def run(self):
+        click.echo("==> Executing")
+        for i in range(0, len(self.steps)):
+            self.execute(i)
+
+    def execute(self, step_number):
+
+        step = self.steps[step_number]
+
+        def get_state(step):
+            """ Return current state """
+            for unit in self.fleet.list_unit_states():
+                if unit.name == step.name:
+                    return unit.systemdSubState
+
+        def is_running():
+            return get_state(step) == 'running'
+
+        # run appropriate action
+        if step.action == 'stop':
+            click.echo("Stopping %s..." % step.name, nl=False)
+            self.fleet.set_unit_desired_state(step.name, 'inactive')
+            while is_running() is True:
+                click.echo('.', nl=False)
+                sleep(1)
+            click.echo("Done.")
+
+        if step.action == 'start':
+            click.echo("Starting %s..." % step.name, nl=False)
+            self.fleet.set_unit_desired_state(step.name, 'launched')
+            while is_running() is False:
+                click.echo('.', nl=False)
+                sleep(1)
+            click.echo("Done.")
 
 
 class BaseDeploymentManager(object):
 
     name = 'Base Deployment'
+    plan = object()
+    units = list()
 
-    def __init__(self, fleet_client, service_name, *args, **kwargs):
+    def __init__(self, fleet_client, service_name, tag, instances, chunk, chunk_percent):
+
         self.fleet = fleet_client
-        self.service_name = service_name
-        # find list of relevant units
-        self.units = list()
+
+        # find relevant units
         for u in self.fleet.list_units():
             if u['name'].startswith(service_name):
-                unit = Instance(fleet_client, u['name'])
+                unit = Instance(u['name'], u['currentState'])
                 self.units.append(unit)
         if len(self.units) == 0:
             raise Exception('No units found')
+
+        self.service_name = service_name
+        self.tag = tag
+
+        # update chunk_count based on our parameters and the number of units
+        if chunk_percent is not None:
+            count = len(self.units)
+            self.chunk_count = int(math.ceil((float(len(self.units))*(float(chunk_percent)/float(100)))))
+        else:
+            if chunk is None:
+                self.chunk_count = len(self.units)  # default to all
+            else:
+                self.chunk_count = chunk
+
+        if instances is None:
+            # assume desired is current state
+            self.desired_instances = len(self.units)
+        else:
+            self.desired_instances = instances
+            if len(self.units) != instances:
+                raise NotImplementedError("Changing the number of instances is not implemented %s found" % len(self.units))
+                # TODO: add or remove the units, and update self.units.
+
+    def create_plan(self):
+        raise NotImplementedError('Method must be implemented in subclass')
+
+    def describe_plan(self):
+        # let us know what will be done, if anything
+        unit_difference = self.desired_instances - len(self.units)
+        if unit_difference > 0:
+            click.echo("Insufficient units found. %s will be spawned." % unit_difference)
+        if unit_difference < 0:
+            click.echo("Excess units found. %s will be destroyed." % unit_difference)
+
+        click.echo("*** %s Deployment Plan ***" % self.name)
+
+        click.echo("==> Details")
+        for u in self.units:
+            click.echo("Found unit: %s (%s)." % (u.name, u.state))
+        click.echo("Chunking: %s units" % self.chunk_count)
+
+        click.echo("==> Steps")
+        i = 0
+        for step in self.plan.steps:
+            i += 1
+            click.echo("Step %s: %s" % (i, step))
+
+    def run(self):
+        self.plan.run()
+        click.echo("Finished.")
 
 
 class SimpleDeployment(BaseDeploymentManager):
     """ Simple Deployment: just stop and start all units """
 
-    name = 'Simple'
-
-    def stop_wait_start(self, from_idx=0, to_idx=None):
-        """ Stop and Wait for unit to start """
-        print("%s Deploy of Unit Ranges: %s-%s" % (self.name, from_idx, to_idx))
-        for unit in self.units[from_idx:to_idx]:
-            unit.stop()
-
-        for unit in self.units[from_idx:to_idx]:
-            print("Stopping %s..." % unit.name, end='')
-            while unit.is_running is True:
-                print('.', end='')
-                sys.stdout.flush()
-                sleep(1)
-            print("Done.")
-
-        for unit in self.units[from_idx:to_idx]:
-            unit.start()
-
-        for unit in self.units[from_idx:to_idx]:
-            print("Starting %s..." % unit.name, end='')
-            while unit.is_running is False:
-                print('.', end='')
-                sys.stdout.flush()
-                sleep(1)
-            print("Done.")
-
-    def run(self):
-        # Stop, Wait, Start all...
-        self.stop_wait_start()
-
-
-class FiftyFiftyDeployment(SimpleDeployment):
-    """ 50/50: stop and restart half, then the other half """
-
-    name = 'Fifty Fifty'
-
-    def run(self):
-        count = len(self.units)
-        fifty = int(math.ceil(len(self.units)/2))
-        # print("Count: %s, Half:%s" % (count, fifty))
-        self.stop_wait_start(0, fifty)
-        self.stop_wait_start(fifty, count)
-
-
-class SpawnDeployment(SimpleDeployment):
-    """ Spawn: spawn new units, then destroy the old. """
-
-    name = 'Spawning'
+    name = 'Stop Start'
 
     def __init__(self, *args, **kwargs):
-        raise NotImplementedError('Not yet Implemented')
-        super(SpawnDeployment, self).__init__(*args, **kwargs)
+        super(SimpleDeployment, self).__init__(*args, **kwargs)
+
+    def create_plan(self):
+        steps = list()
+        for unit in self.units:
+            steps.append(Step('stop', unit.name))
+        for unit in self.units:
+            steps.append(Step('start', unit.name))
+        self.plan = PlanManager(self.fleet, steps)
+
+
+class RollingDeployment(BaseDeploymentManager):
+    """ Rolling Deployment: Start and Stop units by chunking """
+
+    name = 'Rolling'
+
+    def create_plan(self):
+        steps = list()
+        i = 0
+        while i < len(self.units):
+            from_idx = i
+            to_idx = i + self.chunk_count
+            if to_idx > len(self.units):
+                to_idx = len(self.units)
+            for unit in self.units[from_idx:to_idx]:
+                steps.append(Step('stop', unit.name))
+            for unit in self.units[from_idx:to_idx]:
+                steps.append(Step('start', unit.name))
+            i = to_idx
+        self.plan = PlanManager(self.fleet, steps)
+
+
+class AtomicRollingDeployment(RollingDeployment):
+    """ Atomic Deployment: Start new units and destroy old units by chunking """
+
+    name = 'Atomic'
+
+    # configurable by number of containers or %age.
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError('Atomic deployment not yet implemented')
+        super(SimpleDeployment, self).__init__(*args, **kwargs)
 
 
 @click.command()
 @click.option('--fleet-endpoint', default='http+unix://%2Fvar%2Frun%2Ffleet.sock', help="Fleet URI / socket", envvar='FLEETCTL_ENDPOINT')
 @click.option('--name', required=True, help="Name of service to deploy")
-@click.option('--method', default='simple', type=click.Choice(['simple', 'fiftyfifty', 'spawn']), help="Deployment method")
-def main(fleet_endpoint, name, method):
+@click.option('--tag', required=False, type=click.STRING, help="Tag label. eg Git tag")
+@click.option('--method', default='stopstart', type=click.Choice(['stopstart', 'rolling', 'atomic']), help="Deployment method")
+@click.option('--instances', type=click.INT, help="Desired number of instances")
+@click.option('--unit-file', type=click.File(), help="Unit template file")
+@click.option('--chunk', type=click.INT, help="Number or percentage of containers to act on each pass. Eg 2")
+@click.option('--chunk-percent', type=click.INT, help="Percentage of containers to act on each pass. Eg 50")
+@click.option('--delay', default=5, type=click.INT, help="Startup delay")
+def main(fleet_endpoint, name, tag, method, instances, unit_file, chunk, chunk_percent, delay):
     """Main function"""
-    deployment_method_map = {
-        'simple': SimpleDeployment,
-        'fiftyfifty':  FiftyFiftyDeployment,
-        'spawn': SpawnDeployment,
+
+    # Validation
+    if chunk is not None and chunk_percent is not None:
+        raise click.UsageError('Cannot use --chunk and --chuck-percent together.')
+
+    if chunk_percent is not None and not 0 <= chunk_percent <= 100:
+        raise click.UsageError('Invalid --chuck-percent. Valid values 0 - 100.')
+
+    if chunk is not None and chunk > instances:
+        raise click.UsageError('--chunk cannot be greater than --instances.')
+
+    # stopstart validation
+    if method is 'stopstart' and unit_file is not None:
+        raise click.UsageError('--unit-file is not valid for stopstart deployment')
+
+    if method is 'stopstart' and chunk is not None:
+        raise click.UsageError('--chunk is not valid for stopstart deployment')
+
+    if method is 'stopstart' and chunk_percent is not None:
+        raise click.UsageError('--chunk-percent is not valid for stopstart deployment')
+
+    if method is 'stopstart' and instances is not None:
+        raise click.UsageError('--instances is not valid for stopstart deployment')
+
+    if method is 'stopstart' and tag is not None:
+        raise click.UsageError('--tag is not valid for stopstart deployment')
+
+    deployment_map = {
+        'stopstart': SimpleDeployment,
+        'rolling':  RollingDeployment,
+        'atomic': AtomicRollingDeployment,
     }
     connection = FleetConnection(fleet_endpoint)
-    method_obj = deployment_method_map[method]
-    deployment = method_obj(connection, name)
-    deployment.run()
+    method_obj = deployment_map[method]
+    deployment = method_obj(connection, name, tag, instances, chunk, chunk_percent)
+    deployment.create_plan()
+    deployment.describe_plan()  # Print planned execution
+
+    # Give chance to abort
+    click.echo("==> Run")
+    click.echo('Starting in %s seconds...' % delay, nl=False)
+    for i in range(0, delay):
+        sleep(1)
+        click.echo(' %s' % (delay-i), nl=False)
+    click.echo('... Starting.')
+
+    if method is not 'stopstart':
+        deployment.run() #unit_file
+    else:
+        deployment.run()
 
 if __name__ == '__main__':
     main()
