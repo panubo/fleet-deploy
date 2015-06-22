8b0901716f590ed6436dfa9d2334bd797c3ae4ca
Andrew Cutler <macropin@gmail.com>
2015-06-22 15:19:25 +1000

    increase coverage

--- tests/test_plan.py
+++ tests/test_plan.py
@@ -5,11 +5,22 @@ from deploy import Plan, Step
 
 class TestPlan(unittest.TestCase):
 
-    def test_create(self):
-        f = object()
-        u = ''
-        p = Plan(f, u)
-        self.assertEqual(p.__str__(), 'Plan 0')
+    def setUp(self):
+        fleet_client = object()
+        unit_template = ''
+        self.plan = Plan(fleet_client, unit_template)
+
+    def test_str(self):
+        self.assertEqual(self.plan.__str__(), 'Plan 0')
+
+    def test_add_steps(self):
+        self.plan.steps.append(Step('foo', 'destroy'))
+        self.plan.steps.append(Step('bar', 'spawn'))
+        self.assertEqual(len(self.plan.steps), 2)
+
+    def test_execute_external_script(self):
+        self.plan.steps.append(Step('/usr/bin/env', 'external_script'))
+        self.plan.execute(0)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_step.py
+++ tests/test_step.py
@@ -5,15 +5,21 @@ from deploy import Step
 
 class TestStep(unittest.TestCase):
 
+    def setUp(self):
+        self.step = Step('foo', 'stop')
+
     def test_create(self):
-        s = Step('foo', 'stop')
-        self.assertEqual(s.__str__(), 'stop foo')
-        self.assertEqual(s.name, 'foo')
-        self.assertEqual(s.action, 'stop')
+        self.assertEqual(self.step.__str__(), 'stop foo')
+        self.assertEqual(self.step.name, 'foo')
+        self.assertEqual(self.step.action, 'stop')
 
     def test_invalid_action(self):
         with self.assertRaises(Exception):
             s = Step('foo', 'xxx')
+
+    def test_valid_action(self):
+        for action in ('start', 'stop', 'spawn', 'destroy', 'external_script'):
+            s = Step('foo', action)
 
 if __name__ == '__main__':
     unittest.main()
