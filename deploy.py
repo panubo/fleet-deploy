b327a5b3b9b4c387d9ea654dd3c4e3671df96321
Andrew Cutler <macropin@gmail.com>
2015-09-01 11:47:54 +1000

    add support for dead units

--- deploy.py
+++ deploy.py
@@ -39,7 +39,7 @@ class Unit(object):
         self.state = state
         self.required_action = required_action
 
-        if state not in ('inactive', 'launched', 'uncreated'):
+        if state not in ('inactive', 'launched', 'dead', 'uncreated'):
             raise Exception('Invalid state')
 
         if required_action not in ('redeploy', 'spawn', 'destroy'):
